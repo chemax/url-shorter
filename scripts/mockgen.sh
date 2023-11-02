95a14dbf85173ffb0d92d1727e454e3458d729d7
Maksim Chornopolskiy <chemax.kr@gmail.com>
2023-11-02 22:23:47 +0700

    mocks

--- scripts/mockgen.sh
+++ scripts/mockgen.sh
@@ -2,3 +2,4 @@ mockgen -source=./interfaces/interfaces.go -destination=./mocks/storage/storage_
 mockgen -source=./interfaces/interfaces.go -destination=./mocks/db/db_mock.go DBInterface
 mockgen -source=./interfaces/interfaces.go -destination=./mocks/logger/log_mock.go LoggerInterface
 mockgen -source=./interfaces/interfaces.go -destination=./mocks/config/config_mock.go ConfigInterface
+mockgen -source=./interfaces/interfaces.go -destination=./mocks/users/users_mock.go UsersInterface
