cb59fc0189bfae0a7e5814a50613746dd9e6e799
Maksim Chornopolskiy <chemax.kr@gmail.com>
2024-02-27 22:35:36 +0700

    delete test

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -1,20 +1,32 @@
 package app
 
 import (
+	"fmt"
+	"github.com/chemax/url-shorter/models"
+	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
+	"time"
 )
 
 func TestApp(t *testing.T) {
-	//t.Run("ServerAddressEnv", func(t *testing.T) {
-	//	os.Setenv(models.ServerAddressEnv, "fdsfffwefew")
-	//	err := Run()
-	//	assert.Error(t, fmt.Errorf("need address in a form Host:Port"), err)
-	//	os.Unsetenv(models.ServerAddressEnv)
-	//})
-	//t.Run("BaseURLEnv", func(t *testing.T) {
-	//	os.Setenv(models.BaseURLEnv, "http://www.example.com:xxxx//////path")
-	//	err := Run()
-	//	assert.Error(t, err)
-	//	os.Unsetenv(models.BaseURLEnv)
-	//})
+	go func() {
+		// Я не знаю как ещё это протестировать
+		t := time.NewTicker(time.Second * 10)
+		<-t.C
+		os.Exit(0)
+	}()
+	t.Run("ServerAddressEnv", func(t *testing.T) {
+
+		os.Setenv(models.ServerAddressEnv, "fdsfffwefew")
+		err := Run()
+		assert.Error(t, fmt.Errorf("need address in a form Host:Port"), err)
+		os.Unsetenv(models.ServerAddressEnv)
+	})
+	t.Run("BaseURLEnv", func(t *testing.T) {
+		os.Setenv(models.BaseURLEnv, "http://www.example.com:xxxx//////path")
+		err := Run()
+		assert.Error(t, err)
+		os.Unsetenv(models.BaseURLEnv)
+	})
 }
